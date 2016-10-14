2e5a07833675be32dfa1d10f15155cee415b47e4
Ryan-Anderson <ryan@perfectsensedigital.com>
2016-10-14 16:30:18 -0400

    Adds logger

--- deploy.sh
+++ deploy.sh
@@ -9,9 +9,8 @@ make html
 
 # S3 Deploy
 sudo pip install awscli
+echo "Deploying to bucket: $AWS_BUCKET"
 aws s3 sync _build/html s3://$AWS_BUCKET/latest
-
-
 
 # if [[ "$TRAVIS_REPO_SLUG" == "perfectsense/"* ]] && \
 #    [ "$TRAVIS_PULL_REQUEST" == "false" ]; then
